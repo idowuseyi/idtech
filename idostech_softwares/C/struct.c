dad11d5947f4ef9524ab067861759f9169980a3b
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2022-08-02 15:07:11 +0100

    working with typedef

--- idostech_softwares/C/struct.c
+++ idostech_softwares/C/struct.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 struct Player
 {
     char name[12];
     int score; 
-}
+};
 
 
 int main(void)
@@ -14,4 +15,22 @@ int main(void)
     //          listed under one name in a block of memory  
     //          VERY SIMILAR to classes in other languages (but no methods)
     
+
+    struct Player player1;
+    struct Player player2;
+
+    strcpy(player1.name, "Seyi");
+    player1.score = 3;
+
+    strcpy(player2.name, "Femi");
+    player2.score = 7;
+
+    printf("%s\n", player1.name);
+    printf("%d\n", player1.score);
+
+    printf("%s\n", player2.name);
+    printf("%d\n", player2.score);
+
+    return (0);
+
 }
