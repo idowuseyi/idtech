4992ce6a4b1cbc03997a1aa46d4ec9d7b0021f68
idowuseyi <idowuseyi22@gmail.com>
2022-09-10 01:14:04 +0100

    adding new files

---
+++ idostech_softwares/C/advc/1darray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[] = {'I', 'D', 'O', 'W', 'U'};
+	char surname[] = {"IDOWU"};
+
+	printf("sizeof str = %lu\n", sizeof(str));
+	printf("sizeof surname = %lu\n", sizeof(surname));
+	printf("strlen str = %lu\n", sizeof(str));
+	printf("strlen surname = %lu\n", sizeof(surname));
+}
