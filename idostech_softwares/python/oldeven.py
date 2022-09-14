1c8d9ca4140ebb42cf0eaf4b296cfd576a5812ef
idowuseyi <idowuseyi22@gmail.com>
2022-09-14 19:24:24 +0100

    adding new files

--- idostech_softwares/python/oldeven.py
+++ idostech_softwares/python/oldeven.py
@@ -1,7 +1,6 @@
 print ('This program check even or odd number')
-numb
-input ('Input any whole number:' numb)
-if numb % 2 == 0
+numb = int (input ('Input any whole number:'))
+if numb%2 == 0
 print ('Your input is even')
 else
 print ('Your input is odd')
