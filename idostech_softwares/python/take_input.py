85d0aa73bcf63a0bb6c31c0971f1d9210d9f50d8
idowuseyi <idowuseyi22@gmail.com>
2022-09-15 20:39:39 +0100

    creating with IDTECH

--- idostech_softwares/python/take_input.py
+++ idostech_softwares/python/take_input.py
@@ -2,8 +2,8 @@
 
 name = input('What is your name? ')
 color = input('What is your favourite color? ')
-print('Hi ' + name)
-print(name + ' likes' + color)
+print('This is  ' + name)
+print(name + ' likes ' + color)
 
 
 
