286f61605b7a7e4ce7d348fa8ea39806e096f69e
idowuseyi <idowuseyi22@gmail.com>
2022-09-15 21:01:07 +0100

    creating with IDTECH

--- idostech_softwares/python/calc_my_age.py
+++ idostech_softwares/python/calc_my_age.py
@@ -1,9 +1,9 @@
 #This program calculate your age
 
 birth_year = (input('Birth year: '))
-print('data type of input ' + type(birth_year))
+print('data type of input ' type(birth_year))
 #any input is astring
 age = 2022 - int(birth_year)
 #type conversion has been carried out here
-print('data type of age > ' + type(age))
+print('data type of age > ' type(age))
 print(age)
