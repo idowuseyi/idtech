7a7e531719539b473f75150deee66f148ac6a619
idowuseyi <idowuseyi22@gmail.com>
2022-09-16 04:06:03 +0100

    creating with IDTECH

--- idostech_softwares/python/convert.py
+++ idostech_softwares/python/convert.py
@@ -2,4 +2,13 @@
 
 weight = input('Input your weight')
 
-weight_unit =
+weight_unit = input("(L)bs or (K)g: ")
+
+if weight_unit == 'L' or 'l':
+    weight /= 2.2046
+    print(f"You are {weight} kg ")
+elif weight_unit == 'K' or 'k':
+    weight *= 2.2046
+    print(f"You are {weight} kg")
+else:
+    print("Input the right weight unit")
