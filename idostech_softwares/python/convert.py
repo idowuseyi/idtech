107935d30e644bd1651e0fe35042a1ca16b8463f
idowuseyi <idowuseyi22@gmail.com>
2022-09-16 04:21:02 +0100

    creating with IDTECH

--- idostech_softwares/python/convert.py
+++ idostech_softwares/python/convert.py
@@ -4,10 +4,10 @@ weight = int(input('Input your weight: '))
 
 weight_unit = input("(L)bs or (K)g: ")
 
-if weight_unit == "L" or "l":
+if weight_unit.upper() == "L":
     weight /= 2.2
     print(f"You are {weight} kg ")
-elif weight_unit == "K" or "k":
+elif weight_unit.upper() == "K"
     weight *= 2.2
     print(f"You are {weight} pounds")
 else:
