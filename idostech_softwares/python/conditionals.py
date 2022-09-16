84be70cba6ffecc581d4dc2e3d6c0c5ff4a2983a
idowuseyi <idowuseyi22@gmail.com>
2022-09-16 03:08:26 +0100

    creating with IDTECH

--- idostech_softwares/python/conditionals.py
+++ idostech_softwares/python/conditionals.py
@@ -6,7 +6,7 @@ is_cold = True
 if is_hot:
     print("It's a hot day")
     print("Drink plenty of water")
-elif is_cold
+elif is_cold:
     print("It's a cold day")
     print("Wear something warm")
 else:
