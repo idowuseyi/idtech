649c58b71cc5bf2f686d92e4cf631b60be08c5f8
idowuseyi <idowuseyi22@gmail.com>
2022-09-16 03:39:40 +0100

    creating with IDTECH

--- idostech_softwares/python/conditions.py
+++ idostech_softwares/python/conditions.py
@@ -1,3 +1,8 @@
 #character input check
 
 name = input('Enter your name ')
+
+if len(name) < 3:
+    print("name must be at least 3 characters")
+elif len(name) > 50:
+    print("name can only a maximum of 50characters")
