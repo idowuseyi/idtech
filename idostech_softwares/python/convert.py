7b31750627bc86fb26024b95d5904ce7d4effac2
idowuseyi <idowuseyi22@gmail.com>
2022-09-16 04:10:38 +0100

    creating with IDTECH

--- idostech_softwares/python/convert.py
+++ idostech_softwares/python/convert.py
@@ -1,14 +1,14 @@
 # This program takes user input weight and convert it to the other unit
 
-weight = int(input('Input your weight'))
+weight = int(input('Input your weight: '))
 
 weight_unit = input("(L)bs or (K)g: ")
 
 if weight_unit == 'L' or 'l':
-    weight /= 2.2046
+    weight /= 2.2
     print(f"You are {weight} kg ")
 elif weight_unit == 'K' or 'k':
-    weight *= 2.2046
+    weight *= 2.2
     print(f"You are {weight} kg")
 else:
     print("Input the right weight unit")
