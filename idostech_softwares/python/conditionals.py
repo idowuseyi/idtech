de48d0d5885e891a12ee5fe5da9bd81ea176bf32
idowuseyi <idowuseyi22@gmail.com>
2022-09-16 03:28:23 +0100

    creating with IDTECH

--- idostech_softwares/python/conditionals.py
+++ idostech_softwares/python/conditionals.py
@@ -28,5 +28,9 @@ else:
 has_high_income = True
 has_good_credit = True
 
-if has_high_income and has_good_credit
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+if has_high_income and not has_good_credit:
     print("Eligible for loan")
