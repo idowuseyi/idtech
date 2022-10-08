9d055085e857e92fa9747f6456b85c6c04bdddb4
idowuseyi <idowuseyi22@gmail.com>
2022-10-07 18:31:37 -0700

    classes

---
+++ idostech_softwares/python/class_constructor.py
@@ -0,0 +1,14 @@
+'''
+We can use constructor function to define what happens when our class is called
+'''
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print('move')
+
+    def draw(self):
+        print('draw')
