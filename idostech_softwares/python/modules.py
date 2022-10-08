8022aee18d75ba338c0957c7a30e67cc211a2c19
idowuseyi <idowuseyi22@gmail.com>
2022-10-08 09:45:54 -0700

    modules

--- idostech_softwares/python/modules.py
+++ idostech_softwares/python/modules.py
@@ -3,8 +3,4 @@ we use module to section our code. Just like sectioning in a supermarket
 we use it to break our code into multiple sections
 '''
 
-def lbs_to_kg(weight):
-    return weight * 0.45
 
-def kg_to_lbs(weight):
-    return weight / 0.45
