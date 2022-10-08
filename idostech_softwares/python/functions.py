6d4af22ad705a47a2166c718b2a38a22bd010898
idowuseyi <idowuseyi22@gmail.com>
2022-10-07 17:32:26 -0700

    pythonic

--- idostech_softwares/python/functions.py
+++ idostech_softwares/python/functions.py
@@ -1,7 +1,12 @@
-def greet_user():
-    print('Hi there!')
+def greet_user(name):
+    print(f'Hi {name}!')
     print('Welcome aboard')
 
 print("Start")
+greet_user("Ade")
 greet_user()
 print("Finish")
+'''
+:parameter are the items we supply withing our function declaration
+while arguments are the actual value we input into our function at the call of our function
+'''
