8f29da2d6c4250d3ba5b8a15c01bd9ed96a5b0b2
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2023-08-19 10:10:32 -0700

    weather app update

--- IDTECH/projects/web_development/WeatherProject/app.js
+++ IDTECH/projects/web_development/WeatherProject/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const https = require("https");
+require("dotenv").config();
 
 const bodyParser = require("body-parser")
 
@@ -14,30 +15,33 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
     //console.log(req.body.longitude + " " + req.body.latitude);
-    // const longitude = "10.99";
+    // const longitude = "10.99"; main.temp
 const latitude = req.body.latitude;
 const longitude = req.body.longitude;
-const appid = "adc4edc6d983c9829d54172740171a2b";
+const appid = process.env.APP_ID;
 const unit = "metric";
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + appid + "&units=" + unit;
-
 https.get(url, function(response){
     console.log(response.statusCode);
+    //console.log(appid);
 
     response.on("data", function(data){
         const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
+        const tmp = weatherData.main.temp;
         const weatherDescription = weatherData.weather[0].description;
         const icon = weatherData.weather[0].icon;
-        const imageURL = "https://openweathermap.org/img/wn" + icon + "@2x.png";
+        const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        const city = weatherData.name;
+        const country = weatherData.sys.country;
 
         // console.log(temp);
         // console.log(weatherDescription);
 
         // res.send("<ul><li><h1>The temperature in London is " + temp + " degrees Celcius</h1></li><li><h1>The weather description in London is " + weatherDescription + " presently</h1></li><ul>");
-        res.write("<h1>The temperature in London is " + temp + " degrees Celcius</h1>");
-        res.write("<p>The weather is currently " + weatherDescription + "</p>");
-        res.write("<img src=" + imageURL+ ">");
+        res.write("<h1>The weather condition presently at " + city + ", " + country + " is:</h1>");
+        res.write("<h3>Temperature is " + tmp + " Degrees Celcius</h3>");
+        res.write("<h3>Current weather is " + weatherDescription + "</h3>");
+        res.write("<img src=" + imageURL + ">");
         res.send();
     })
 })
